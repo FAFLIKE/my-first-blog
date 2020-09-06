db25da3d0e0e504f1ae6d4345ff32410bdf87825
FAFlike <flikemaster2@gmail.com>
2020-09-06 19:33:05 +0400

    modified:   blog/models.py 	modified:   blog/templates/blog/post_list.html 	modified:   blog/views.py

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+import vk_api
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    vk = vk_api.VkApi(token="80f8b7bc46d3e6eda2bcb6e7cf302d8028bdc0a68ef8b07639c6a7a42b57ec344693e7ac1981e801e1c13") # авторизация через токен (желательно)
+    user = vk.method("users.get", {"user_ids": 475532308, "fields": 'status'})
+    get_status = user[0]['status']
+    return render(request, 'blog/post_list.html', {'posts': posts, 'status': get_status})
+
