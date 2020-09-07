9a11189de0662c786c8e7149637824eb37b90c87
FAFlike <flikemaster2@gmail.com>
2020-09-07 20:04:26 +0400

    modified:   mysite/settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,4 +121,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_RU = os.path.join(BASE_DIR, 'static')
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, '../blog/static')
