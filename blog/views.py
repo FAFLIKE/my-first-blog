89db455fc049378706b2e603873a5e24a5f986dc
FAFlike <flikemaster2@gmail.com>
2020-09-07 19:49:43 +0400

    Есть отдельные страницы

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 import vk_api
@@ -10,3 +10,6 @@ def post_list(request):
     get_status = user[0]['status']
     return render(request, 'blog/post_list.html', {'posts': posts, 'status': get_status})
 
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
